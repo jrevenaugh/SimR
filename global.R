92b333b92218af31f1f11be5f0c177321d378668
Justin Revenaugh <justinr@umn.edu>
2018-05-04 20:49:58 -0500

    Added some hashes for vertices to edges and vertices to triangles.

--- global.R
+++ global.R
@@ -5,7 +5,7 @@ require(tidyverse)
 # Geometry
 phi <- seq(pi, -pi, length.out = 7)
 radius <- 1
-Ematrix <- matrix(0, nrow = 40, ncol = 2)
+Ematrix <- matrix(0, nrow = 30, ncol = 2)
 k <- 1
 for (i in 1:5) {
   for (j in (i + 1):6) {
@@ -17,10 +17,38 @@ for (i in 1:5) {
 edges <- as.data.frame(Ematrix)
 colnames(edges) <- c("x", "y")
 edges$player <- "gray"
-edges$seq <- rep( seq(1, 20), each = 2)
+edges$seq <- rep( seq(1, 15), each = 2)
 
 vertices <- radius * data.frame(x = cos(phi), y = sin(phi)) %>% slice(1:6)
 vertices$names <- toupper(letters[1:6])
+
+# Hash for edges lookup (row,column = vertices, 1 = A)
+v2e <- matrix(0, nrow = 6, ncol = 6)
+k <- 1
+for (i in 1:5) {
+  for (j in (i + 1):6) {
+    v2e[i,j] <- k
+    v2e[j,i] <- k
+    k <- k + 1
+  }
+}
+
+# Hash from vertices to triangle
+v2t <- array(0, dim = c(6, 6, 6))
+l <- 1
+for (i in 1:4) {
+  for (j in (i + 1):5) {
+    for (k in (j + 1):6) {
+      v2t[i,j,k] <- l
+      v2t[j,i,k] <- l
+      v2t[i,k,j] <- l
+      v2t[k,i,j] <- l
+      v2t[j,k,i] <- l
+      v2t[k,j,i] <- l
+      l <- l + 1
+    }
+  }
+}
 
 
 # Graphics ---------------------------------------------------------------------
